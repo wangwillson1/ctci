5e32e5f5551f14d3e3c9a791236ee14f38b99c6b
Willson Wang <40642117+wangwillson1@users.noreply.github.com>
2019-12-27 20:41:53 -0500

    is_unique

--- ch1/isunique.cc
+++ ch1/isunique.cc
@@ -2,3 +2,41 @@
 // unique characters
 // is_unique2 does the same as is_unique but does not use additional
 // data structures
+
+#include <iostream>
+#include <string>
+#include <map>
+
+bool is_unique(std::string str) {
+  int len = str.length();
+  std::map<char, bool> chars;
+
+  for (int i = 0; i < len; i++) {
+    if (chars.count(str[i]) != 0) return false;
+
+    else {
+      chars[str[i]] = true;
+    }
+  }
+
+  return true;
+}
+
+bool is_unique2(std::string str) {
+  int len = str.length();
+
+  for (int i = 0; i < len; i++) {
+    for (int j = i; j < len; j++) {
+      if (str[i] == str[j]) return false;
+    }
+  }
+
+  return true;
+}
+
+int main() {
+  std::string str1 = "abcde";
+  std::string str2 = "abcdee";
+
+  std::cout << is_unique(str1) << is_unique(str2) << std::endl;
+}
